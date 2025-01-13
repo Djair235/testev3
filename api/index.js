afce6f775d99529d99f1518cb236a41afe0a9152
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-13 19:59:02 -0300

    Update and rename index.js to api/index.js

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = ("../router/router")
+const app = express()
+
+app.use(router)
+
+app.listen(3000, () => {
+    console.log("Server rodando na porta 3000")
+})
+
+module.exports = app
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-const router = express.Router()
-
-router.get("/", (req, res) => {
-    res.send("Router e Vercel estão funcionando!")
-})
-
-module.exports = router
