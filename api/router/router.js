f95ebb9ff47043b2f57191dbd2abc9d45bc7e890
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-14 00:23:45 -0300

    Update router.js

--- api/router/router.js
+++ api/router/router.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const getModel = require("../models/getModel")
 const app = express()
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("Router e Vercel estão funcionando.")
-})
+// router.get("/", (req, res) => {
+//     res.send("Router e Vercel estão funcionando!")
+// })
+
+router.get("/", getModel.getAll)
 
 module.exports = router
