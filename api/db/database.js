1410f8bad8192bfee3a54ad828ab80bd2aa1bc9a
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-14 00:30:07 -0300

    Update database.js

--- api/db/database.js
+++ api/db/database.js
@@ -4,7 +4,10 @@ const connection = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 5, // Limite de conexões no pool
+    queueLimit: 0   
 })
 
 module.exports = connection
