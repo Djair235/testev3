51bd2dc9e7a444557f065064de3e87e232174ddf
djair.fts <140333747+Djair235@users.noreply.github.com>
2025-01-14 00:23:01 -0300

    Create getModel.js

---
+++ api/models/getModel.js
@@ -0,0 +1,13 @@
+const connection = require("../db/database")
+
+async function getAll(request, response) {
+    try {
+        const [rows] = await connection.execute("SELECT * from CLIENTES")
+        return response.status(200).json(rows)
+    } catch (error) {
+        console.error(`Erro ao buscar os clientes. ${error}`)
+        return response.status(400).json({erro: "Erro interno ao ao processar a requisição"})
+    }
+}
+
+module.exports = { getAll }
